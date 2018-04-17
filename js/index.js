f347bd2c48a5f30b87a92b8e205cf3ce53638691
Scott Lieber <WSox1235@aol.com>
2018-04-17 11:02:16 -0400

    first commit javascript page

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$("img", "#gameoflife").hover(function() {
+  $('.gamestill').toggle();
+  $('.gamegif').toggle();
+});
+
+$("img", "#simon").hover(function() {
+  $('.simonstill').toggle();
+  $('.simongif').toggle();
+});
+
+$("img", "#temp").hover(function() {
+  $('.tempstill').toggle();
+  $('.tempgif').toggle();
+});
+
+$("img", "#gdp").hover(function() {
+  $('.gdpstill').toggle();
+  $('.gdpgif').toggle();
+});
+
+$("img", "#nytimes").hover(function() {
+  $('.nytstill').toggle();
+  $('.nytgif').toggle();
+});
+
+$("img", "#educ").hover(function() {
+  $('.educstill').toggle();
+  $('.educgif').toggle();
+});
